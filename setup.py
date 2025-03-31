4c378112a0d4d034a05570e9f7be1284620210a2
uv547756 <uv547756@gmai.com>
2025-03-31 17:56:41 +0530

    Added bianries for the game

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ executables = [cx_Freeze.Executable('main.py', base="Win32GUI")]
 cx_Freeze.setup(
     name = "FlappyBird",
     options = {
-        "build.exe" : {"packages" : ["pygame"], "include_files": ["assets", "ComicSans.ttf"]}},
+        "build_exe" : {"packages" : ["pygame"], "include_files": ["assets"]}},
         executables = executables    
 )
